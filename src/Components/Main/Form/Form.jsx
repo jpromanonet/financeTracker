5bcf6f7af845557308dd343853ebbc57c0b513dc
jpromanonet <programming@jpromano.net>
2021-12-26 23:10:28 -0300

    Form.jsx

--- src/Components/Main/Form/Form.jsx
+++ src/Components/Main/Form/Form.jsx
@@ -19,7 +19,7 @@ const initialState = {
 const NewTransactionForm = () => {
     const classes = useStyles();
     const { addTransaction } = useContext(ExpenseTrackerContext);
-    const [formDate, setFormData] = useState(initialState);
+    const [formData, setFormData] = useState(initialState);
     const { segment } = useSpeechContext();
     const [open, setOpen] = React.useState(false);
 
@@ -37,5 +37,36 @@ const NewTransactionForm = () => {
         setFormData(initialState);
     };
 
-    useEffect
+    useEffect(() => {
+        if(segment) {
+            if(segment.intent.intent === 'add_expense') {
+                setFormData({...formData, type: 'Expense'});
+            } else if (segment.intent.intent === 'add_income') {
+                setFormData({...formData, type: 'Income'});
+            } else if (segment.isFinal && segment.intent.intent === 'create_transaction') {
+                return createTransaction();
+            } else if (segment.isFinal && segment.intent.intent === 'cancel_transaction') {
+                return setFormData(initialState);
+            }
+
+            segment.entities.forEach((s) => {
+                const category = `${s.value.charAt(0)}${s.value.slice(1).toLocaleLowerCase()}`;
+
+                switch (s.type) {
+                    case 'amount':
+                        setFormData({...formData, amount: s.value});
+                        break;
+                    case 'category':
+                        if(incomeCategories.map((iC) => iC.type).includes(category)) {
+                            setFormData({...formData, type: 'Income', category});
+                        } else if (expenseCategories.map((iC) => iC.type).includes(category)) {
+                            setFormData({...formData, type: 'Expense', category});
+                        }
+                        break;
+                    case 'date':
+                        
+                }
+            })
+        }
+    })
 }
